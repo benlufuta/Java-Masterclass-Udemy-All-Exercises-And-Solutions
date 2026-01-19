9fd64bd2ad8b01ca703ee83410231c8a4abd74fd
Ben Lufuta <ben.lufuta@gmail.com>
2026-01-19 14:32:37 -0500

    Solution for Exercise 8

---
+++ Exercises 08 - Teen Number Checker/TeenNumberChecker.java
@@ -0,0 +1,12 @@
+public class TeenNumberChecker {
+    
+    public static boolean isTeen(int age) {
+        
+        return (age >= 13 && age <= 19);
+    }
+    
+    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge){
+        
+        return isTeen(firstAge) || isTeen(secondAge) || isTeen(thirdAge);
+    }
+}
